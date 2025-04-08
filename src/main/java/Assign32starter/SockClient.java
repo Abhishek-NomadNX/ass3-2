99a83c7aae205d03fe18ee18132851f91a20989a
Abhishek-NomadNX <abhishek.srivastava.nomadnx@gmail.com>
2025-04-09 01:50:47 +0530

    Created basic

---
+++ src/main/java/Assign32starter/SockClient.java
@@ -0,0 +1,121 @@
+package Assign32starter;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+
+/**
+ *
+ */
+class SockClient {
+    static Socket sock = null;
+    static String host = "localhost";
+    static int port = 8888;
+    static OutputStream out;
+    // Using and Object Stream here and a Data Stream as return. Could both be the same type I just wanted
+    // to show the difference. Do not change these types.
+    static ObjectOutputStream os;
+    static DataInputStream in;
+
+    public static void main(String args[]) {
+
+        if (args.length != 2) {
+            System.out.println("Expected arguments: <host(String)> <port(int)>");
+            System.exit(1);
+        }
+
+        try {
+            host = args[0];
+            port = Integer.parseInt(args[1]);
+        } catch (NumberFormatException nfe) {
+            System.out.println("[Port|sleepDelay] must be an integer");
+            System.exit(2);
+        }
+
+        try {
+            connect(host, port); // connecting to server
+            System.out.println("Client connected to server.");
+            boolean requesting = true;
+            Scanner scanner = new Scanner(System.in);
+            while (requesting) {
+                System.out.println("\nChoose action:");
+                System.out.println("1 - start\n2 - guess\n3 - next\n4 - skip\n5 - remaining\n0 - quit");
+                int choice = Integer.parseInt(scanner.nextLine());
+                JSONObject request = new JSONObject();
+
+                switch (choice) {
+                    case 1:
+                        System.out.println("Enter your name");
+                        String username = scanner.nextLine();
+
+                        System.out.println("Enter duration");
+                        String duration = scanner.nextLine();
+
+                        request.put("action", "start");
+                        request.put("name", username);
+                        request.put("duration", duration);
+
+                        break;
+                    case 2:
+                        System.out.println("Enter your movie guess: ");
+                        String guess = scanner.nextLine();
+
+                        request.put("action", "guess");
+                        request.put("answer", guess);
+                        break;
+                    case 3:
+                        request.put("action", "next");
+                        break;
+                    case 4:
+                        request.put("action", "skip");
+                        break;
+                    case 5:
+                        request.put("action", "remaining");
+                        break;
+                    case 0:
+                        request.put("action", "quit");
+                        requesting = false;
+                        break;
+                    default:
+                        System.out.println("Invalid option. Try again.");
+                        continue;
+                }
+                // Send request
+                System.out.println(request);
+                os.writeObject(request.toString());
+                os.flush();
+
+
+                // Receive response
+                String responseStr = in.readUTF();
+
+                System.out.println(responseStr);
+
+                if (!requesting) {
+                    System.out.println("Game ended. Thanks for playing!");
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error e");
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void connect(String host, int port) throws IOException {
+        // open the connection
+        sock = new Socket(host, port); // connect to host and socket on port 8888
+
+        // get output channel
+        out = sock.getOutputStream();
+
+        // create an object output writer (Java only)
+        os = new ObjectOutputStream(out);
+
+        in = new DataInputStream(sock.getInputStream());
+    }
+}
--- src/main/java/Assign32starter/SockServer.java
+++ src/main/java/Assign32starter/SockServer.java
@@ -22,8 +22,25 @@ import org.json.*;
 public class SockServer {
     static Stack<String> imageSource = new Stack<String>();
 
+    static DataOutputStream os;
+    static ObjectInputStream in;
+
+    static Map<String, List<String>> movieImages = new LinkedHashMap<>();
+    static List<String> movieOrder = new ArrayList<>();
+    static int currentMovieIndex = 0;
+    static int currentImageIndex = 0;
+    static int skipsLeft = 3;
+    static String duration;
+
+    static boolean isGameRunning = false;
+    static Map<String, Integer> allScores = new LinkedHashMap<>();
+
+    static String currentUser = "";
+    static int score = 0;
+
     public static void main(String args[]) {
         Socket sock;
+
         try {
 
             //opening the socket here, just hard coded since this is just a bas example
@@ -38,36 +55,157 @@ public class SockServer {
             // must cast the object from Object to desired type to be useful
             while (true) {
                 sock = serv.accept(); // blocking wait
+                System.out.println("Client connected");
 
-                // could totally use other input outpur streams here
-                ObjectInputStream in = new ObjectInputStream(sock.getInputStream());
+                // setup the object reading channel
+                in = new ObjectInputStream(sock.getInputStream());
+
+                // get output channel
                 OutputStream out = sock.getOutputStream();
 
-                String s = (String) in.readObject();
-                JSONObject json = new JSONObject(s); // the requests that is received
+                // create an object output writer (Java only)
+                os = new DataOutputStream(out);
 
-                JSONObject response = new JSONObject();
-
-                if (json.getString("type").equals("start")) {
-
-                    System.out.println("- Got a start");
-
-                    response.put("type", "hello");
-                    response.put("value", "Hello, please tell me your name.");
-                    sendImg("img/hi.png", response); // calling a method that will manipulate the image and will make it send ready
-
-                } else {
-                    System.out.println("not sure what you meant");
-                    response.put("type", "error");
-                    response.put("message", "unknown response");
+                while(true) {
+                    try {
+                        String s = (String) in.readObject();
+                        System.out.println("Request : " + s);
+                        JSONObject req = new JSONObject(s);
+                        JSONObject res = handleRequest(req);
+                        writeOut(res);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        JSONObject error = new JSONObject();
+                        error.put("ok", false);
+                        error.put("message", "Error processing request");
+                        writeOut(error);
+                    }
                 }
-                PrintWriter outWrite = new PrintWriter(sock.getOutputStream(), true); // using a PrintWriter here, you could also use and ObjectOutputStream or anything you fancy
-                outWrite.println(response.toString());
             }
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    static JSONObject handleRequest(JSONObject req) throws Exception {
+        String action = req.getString("action");
+        JSONObject res = new JSONObject();
+
+        switch (action) {
+            case "start":
+                statGame(req);
+                res.put("ok", true);
+                res.put("img", getCurrentImage());
+                res.put("msg", "Game Started");
+                break;
+
+            case "guess":
+                String userGuess = req.getString("answer").toLowerCase();
+                String correctMovie = movieOrder.get(currentMovieIndex).toLowerCase();
+                if (userGuess.equals(correctMovie)) {
+                    score++;
+                    res.put("ok", true);
+                    res.put("msg", "Correct! Moving to next movie.");
+                    currentMovieIndex++;
+                    currentImageIndex = 0;
+                } else {
+                    res.put("ok", false);
+                    res.put("msg", "Wrong guess. Try again or type 'next' for clearer image.");
+                }
+
+                if (currentMovieIndex < movieOrder.size()) {
+                    res.put("img", getCurrentImage());
+                } else {
+                    res.put("msg", "Game Over! No more movies.");
+                }
+                break;
+
+            case "next":
+                currentImageIndex++;
+                if (currentImageIndex >= 4) {
+                    currentImageIndex = 3;
+                    res.put("ok", false);
+                    res.put("msg", "This is the clearest image already.");
+                } else {
+                    res.put("ok", true);
+                    res.put("msg", "Here is a clearer image.");
+                }
+                res.put("img", getCurrentImage());
+                break;
+
+            case "skip":
+                if (skipsLeft > 0) {
+                    skipsLeft--;
+                    currentMovieIndex++;
+                    currentImageIndex = 0;
+                    res.put("ok", true);
+                    res.put("msg", "Movie skipped.");
+                    if (currentMovieIndex < movieOrder.size()) {
+                        res.put("img", getCurrentImage());
+                    } else {
+                        res.put("msg", "Game Over! No more movies.");
+                    }
+                } else {
+                    res.put("ok", false);
+                    res.put("msg", "No skips left!");
+                }
+                break;
+
+            case "remaining":
+                res.put("ok", true);
+                res.put("msg", "Skips remaining: " + skipsLeft);
+                break;
+
+            case "quit":
+                res.put("ok", true);
+                res.put("msg", "Goodbye " + currentUser + "! Your score: " + score);
+                break;
+
+            default:
+                res.put("ok", false);
+                res.put("msg", "Unknown command.");
+                break;
+        }
+
+        return res;
+    }
+
+    static void statGame(JSONObject req) {
+        currentUser = req.getString("name");
+        duration = req.getString("duration");
+
+        if(duration.equalsIgnoreCase("short")) {
+            skipsLeft = 2;
+        } else if(duration.equalsIgnoreCase("medium")) {
+            skipsLeft = 4;
+        } else {
+            skipsLeft = 6;
+        }
+
+        currentMovieIndex = 0;
+        currentImageIndex = 0;
+        score = 0;
+        initializeMovies();
+    }
+
+    static void initializeMovies() {
+        movieImages.put("Inception", Arrays.asList("inception1.png", "inception2.png", "inception3.png", "inception4.png"));
+        movieImages.put("Titanic", Arrays.asList("titanic1.png", "titanic2.png", "titanic3.png", "titanic4.png"));
+        movieImages.put("Matrix", Arrays.asList("matrix1.png", "matrix2.png", "matrix3.png", "matrix4.png"));
+        movieImages.put("Avatar", Arrays.asList("avatar1.png", "avatar2.png", "avatar3.png", "avatar4.png"));
+        movieImages.put("Interstellar", Arrays.asList("interstellar1.png", "interstellar2.png", "interstellar3.png", "interstellar4.png"));
+
+        // Create a temporary list of keys, shuffle, then add to movieOrder
+        List<String> movieList = new ArrayList<>(movieImages.keySet());
+        Collections.shuffle(movieList); // Randomize the order
+        movieOrder.addAll(movieList);
+    }
+
+    static String getCurrentImage() {
+        if (currentMovieIndex >= movieOrder.size()) return "none";
+        String movie = movieOrder.get(currentMovieIndex);
+        return movieImages.get(movie).get(currentImageIndex);
     }
 
     /* TODO this is for you to implement, I just put a place holder here */
@@ -81,5 +219,18 @@ public class SockServer {
             obj.put("image", "Pretend I am this image: " + filename);
         }
         return obj;
+    }
+
+    // sends the response and closes the connection between client and server.
+    static void writeOut(JSONObject res) {
+        try {
+            os.writeUTF(res.toString());
+            // make sure it wrote and doesn't get cached in a buffer
+            os.flush();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
